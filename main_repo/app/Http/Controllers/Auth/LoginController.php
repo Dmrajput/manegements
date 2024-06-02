561632fd9b85cb84be18cad61eba7a6260198e92
Divyaraj <divyarajsinh5216@gmail.com>
2024-06-02 15:34:35 +0530

    laravel login api commit

---
+++ main_repo/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json(['access_token' => $token, 'token_type' => 'Bearer'], 200);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
